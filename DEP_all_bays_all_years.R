f159a72f64cc8cf6e994449cc1bb4800cef03e40
Pine <billpine@ufl.edu>
2022-02-22 15:37:56 -0500

    ok lots to update, revised figures and nb.glm first working with the data summed by period and bay. not happy with those glms so then summed all data by period, bay, site (like redfish point) which gave more observations, so to handle those observations I treated site as a random effect in the GLM. Seemed to work ok so then I moved on and made the plots of predicted counts per quadrat in each period for each site along with the observed data.

--- DEP_all_bays_all_years.R
+++ DEP_all_bays_all_years.R
@@ -98,6 +98,15 @@ names(quad_sum) <- c("Period", "Bay",
 # names(spat_sum) <- c("Bay","Period",
 #                      "Number Live Spat")
 
+###########################
+###########################
+#below I sum oysters in each size class by bay and period
+#This combines all of the sites together for a bay and period
+#this increases the number of oysters because the sites
+#are summed, but it reduces the information from each site
+#within a bay
+###########################
+###########################
 
 
 #sum live counts for each transect
@@ -149,8 +158,17 @@ f3<-ggplot(d6, aes(Period, CPUE_Legal)) +
 
 plot_grid(f1,f2,f3)
 
-ggsave("dep_allbays.pdf", width = 10, height = 10)
+#ggsave("dep_allbays_cpue.pdf", width = 10, height = 10)
+
 #############
+#The CPUE was just for visual
+#############
+
+
+#####################################
+#Now I work on some basic nbGLMs using the counts
+#I'm not working with CPUE
+#####################################
 
 #plots of total counts and then GLMs
 f4<-ggplot(d6, aes(Period, Sum_spat)) +
@@ -185,11 +203,19 @@ var(d6$Sum_sublegal)
 mean(d6$Sum_legal)
 var(d6$Sum_legal)
 
+#variances are greater than means for each size class
+#data from all bays combined
+
+#suggests NB
+
 names(d6)
 
 qqnorm(d6$Sum_spat)
 qqnorm(d6$Sum_sublegal)
 qqnorm(d6$Sum_legal)
+
+#qqnorms look overdispersed
+
 
 library(vcd)
 
@@ -212,14 +238,18 @@ d6$Bay <- as.factor(d6$Bay)
 #fit basic NB GLM
 m1 <- glm.nb(Sum_spat ~ Bay + offset(log(Num_quads)), data = d6) 
 m2 <- glm.nb(Sum_spat ~ Bay + Period + offset(log(Num_quads)), data = d6) 
-m3 <- glm.nb(Sum_spat ~ Bay * Period + offset(log(Num_quads)),data = d6, control = glm.control(maxit = 500)
+m3 <- glm.nb(Sum_spat ~ Bay * Period + offset(log(Num_quads)),data = d6, control = glm.control(maxit = 5000)
 ) 
 
 cand.set = list(m1,m2,m3)
 modnames = c("bay", "bay+period", "bay*period")
 aictab(cand.set, modnames, second.ord = FALSE) #model selection table with AIC
 
-summary(m3)
+#so model 3 isn't fitting
+summary(m1)
+summary(m2)
+
+#results suggests for spat bays are different but period isn't
 
 m1.1 <- glm.nb(Sum_sublegal ~ Bay + offset(log(Num_quads)), data = d6) 
 m2.1 <- glm.nb(Sum_sublegal ~ Bay + Period + offset(log(Num_quads)), data = d6) 
@@ -229,7 +259,8 @@ cand.set2 = list(m1.1,m2.1,m3.1)
 modnames2 = c("bay", "bay+period", "bay*period")
 aictab(cand.set2, modnames2, second.ord = FALSE) #model selection table with AIC
 
-summary(m1.1)
+summary(m2.1)
+#again, bays differ for sublegal but time doesn't
 
 #legals
 m1.2 <- glm.nb(Sum_legal ~ Bay + offset(log(Num_quads)), data = d6) 
@@ -241,10 +272,203 @@ modnames3 = c("bay", "bay+period", "bay*period")
 aictab(cand.set3, modnames3, second.ord = FALSE) #model selection table with AIC
 
 summary(m2.2)
+#for legals, st. andrews differs from others but that's it
 
-control = glm.control(maxit = 500)
+##################################################
+##################################################
+###now let's go back and just sum by bay and period and site
+##then try random effects
+##We will treat a site as the random effect, so all the quadrats
+##at a site (such as redfish point) are all assumed to be "related"
+##this differs from the earlier analyses where we summed all the sites
+##################################################
+##################################################
 
-#plot ideas
-https://cran.r-project.org/web/packages/ggiraphExtra/vignettes/ggPredict.html
-#https://mran.microsoft.com/snapshot/2017-04-22/web/packages/sjPlot/vignettes/sjpglm.html
-https://www.middleprofessor.com/files/applied-biostatistics_bookdown/_book/plotting-functions-ggplotsci.html#estimate-response-and-effects-with-emmeans
+
+#sum live counts by bay, period, site 
+count_spat_site=aggregate(Spat~Bay+Period+Site,data=d4,sum)
+count_spat_site <- dplyr::rename(count_spat_site,Bay=Bay,Period=Period,Sum_spat=Spat, Site=Site)
+
+count_sublegal_site=aggregate(Sublegal~Bay+Period+Site,data=d4,sum)
+count_sublegal_site <- dplyr::rename(count_sublegal_site,Bay=Bay,Period=Period,Sum_sublegal=Sublegal,Site=Site)
+
+count_legal_site=aggregate(Legal~Bay+Period+Site,data=d4,sum)
+count_legal_site <- dplyr::rename(count_legal_site,Bay=Bay,Period=Period,Sum_legal=Legal, Site=Site)
+
+#count number quads by doing the length of transect, then rename
+count_quads_site=aggregate(Spat~Bay+Period+Site,data=d4,length)
+count_quads_site <- dplyr::rename(count_quads_site,Bay=Bay,Period=Period,Num_quads=Spat)
+
+#merge spat live count total data frame with the tran_length total data frame
+d7=merge(count_spat_site,count_quads_site,by=c("Bay", "Period", "Site"))
+d7.1=merge(d7,count_sublegal_site,by=c("Bay", "Period", "Site"))
+d7.2=merge(d7.1,count_legal_site,by=c("Bay", "Period", "Site"))
+
+d8<-d7.2
+
+
+names(d8)
+
+d9<-dplyr::select(d8,Bay, Period, Site, Num_quads, 
+               Sum_spat,Sum_sublegal,Sum_legal)
+
+#calculate CPUE. Just for fun to plot
+d9$CPUE_Spat<-d9$Sum_spat/d9$Num_quads
+d9$CPUE_Sublegal<-d9$Sum_sublegal/d9$Num_quads
+d9$CPUE_Legal<-d9$Sum_legal/d9$Num_quads
+
+#the plots below show you the range in CPUE for a given size class
+#of oyster in each of the bays
+
+s1<-ggplot(d9, aes(Period, CPUE_Spat)) +
+  geom_point(size=4) +
+  ggtitle("Spat CPUE by Period") +
+  xlab("Period") +
+  ylab("Spat") +
+  facet_wrap(~Bay)
+
+s2<-ggplot(d9, aes(Period, CPUE_Sublegal)) +
+  geom_point(size=4) +
+  ggtitle("Sublegal CPUE by Period") +
+  xlab("Period") +
+  ylab("Sublegal") +
+  facet_wrap(~Bay)
+
+s3<-ggplot(d9, aes(Period, CPUE_Legal)) +
+  geom_point(size=4) +
+  ggtitle("Legal CPUE by Period") +
+  xlab("Period") +
+  ylab("Legal") +
+  facet_wrap(~Bay)
+
+plot_grid(s1,s2,s3)
+
+ggsave("dep_allbays_site.pdf", width = 10, height = 10)
+
+#single plot of CPUE, spat, different color by Bay
+s4<-ggplot(d9, aes(Period, CPUE_Spat, color=Bay)) +
+  geom_point(size=4) +
+  ggtitle("Spat CPUE by Period") +
+  xlab("Period") +
+  ylab("Spat CPUE")
+
+#########
+##ok move away from CPUE and work on the GLMs
+#########
+
+qqnorm(d9$Sum_spat)
+qqnorm(d9$Sum_sublegal)
+qqnorm(d9$Sum_legal)
+
+#yes overdispersed
+
+#some GLMs
+
+#single plot, sum spat
+#this is the raw data, then you will fit nb.glm to these data 
+#and use sampling site
+#as random effect
+
+r0<-ggplot(d9, aes(Period, Sum_spat)) +
+  geom_point(size=4) +
+  ggtitle("Spat per Period by Site") +
+  xlab("Period") +
+  ylab("Total Spat")+
+  facet_wrap(~Bay)
+
+
+#fit basic NB GLM Random
+
+library(lme4) #mixed effect models
+library(MASS) #negative binomial models
+
+#make site factor for random effect
+d9$Site<-as.factor(d9$Site)
+
+#make bay factor to use in ggpredict as group
+d9$Bay<-as.factor(d9$Bay)
+
+#no offset, station name as random
+r1 <- glmer.nb(Sum_spat ~ Period + (1|Site) + offset(log(Num_quads)), data = d9) #no converge
+r2 <- glmer.nb(Sum_spat ~ Period + Bay + (1|Site) + offset(log(Num_quads)), data = d9) #converge
+
+summary(r2)
+
+library(ggeffects)
+
+ggpredict(r2)
+
+pred_r2 <- ggpredict(r2, c("Period", "Bay"))
+#really important to look at this pred_r2 because it shows you
+#the groups have unique predictions
+
+#plot(pred_r2, facet=TRUE, colors="Bay")
+plot(pred_r2, facet=TRUE, colors=c("red","black","blue"))
+
+#plot(pred_r2, facet=TRUE, colors="social")
+#palletts and options here
+#https://strengejacke.github.io/ggeffects/reference/plot.html#examples
+
+#or a better way is how Jennifer does it
+
+#now subset the predicted for each bay
+app_pred <- subset(pred_r2, pred_r2$group == "Apalachicola")
+pen_pred <- subset(pred_r2, pred_r2$group == "Pensacola")
+sa_pred <- subset(pred_r2, pred_r2$group == "St. Andrews")
+
+#now make new plots for each bay and add the raw data points
+#this gives you predicted for each bay and then the raw data points
+
+pm1 = ggplot(app_pred, aes(x, predicted))+
+  geom_line(size=2)+
+  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .5)+
+  ylab("Live oyster count per quad") +
+  xlab ("Period")+
+  ggtitle("Apalachicola Spat by Period") +
+  geom_point(data = d9[d9$Bay == "Apalachicola",], mapping = aes(Period, Sum_spat), size = 2)
+
+  
+pm2 = ggplot(pen_pred, aes(x, predicted))+
+  geom_line(size=2)+
+  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .5)+
+  ylab("Live oyster count per quad") +
+  xlab ("Period")+
+  ggtitle("Pensacola Spat by Period") +
+  geom_point(data = d9[d9$Bay == "Pensacola",], mapping = aes(Period, Sum_spat), size = 2)
+
+pm3 = ggplot(sa_pred, aes(x, predicted))+
+  geom_line(size=2)+
+  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .5)+
+  ylab("Live oyster count per quad") +
+  xlab ("Period")+
+  ggtitle("St. Andrews Spat by Period") +
+  geom_point(data = d9[d9$Bay == "St. Andrews",], mapping = aes(Period, Sum_spat), size = 2)
+
+plot_grid(r0,pm1,pm2,pm3)
+
+ggsave("dep_allbays_predicted_nbglm.pdf", width = 10, height = 10)
+
+#or
+
+pm <- plot_grid(
+  pm1 + theme(legend.position="none"),
+  pm2 + theme(legend.position="none"),
+  pm3 + theme(legend.position="none"),
+  align = 'vh',
+  hjust = -1,
+  nrow = 1,
+  ncol=3
+)
+
+
+#for plotting could scale y axis better but that would cause 1 point for St. Andrews to be lost
+  
+
+  
+  
+  
+# 
+# #plot ideas
+# https://cran.r-project.org/web/packages/ggiraphExtra/vignettes/ggPredict.html
+# #https://mran.microsoft.com/snapshot/2017-04-22/web/packages/sjPlot/vignettes/sjpglm.html
+# https://www.middleprofessor.com/files/applied-biostatistics_bookdown/_book/plotting-functions-ggplotsci.html#estimate-response-and-effects-with-emmeans
